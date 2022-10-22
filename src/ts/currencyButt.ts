82c7d5a7dec3223246c86de18a21b3595e2f0fac
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-10-22 21:48:08 +0300

    currency menu open func
    Я сделал через ебанный костыль потому что hide не хотел работать, не знаю почему я испробовал всё. Позже обезательно сделаю нормально

---
+++ src/ts/currencyButt.ts
@@ -0,0 +1,18 @@
+(() => {
+    let curr = document.querySelector(".currency") as HTMLInputElement;
+
+    let currBut = curr!.querySelector("button") as HTMLInputElement;
+
+    let currMenu = curr!.querySelector(".currency-menu") as HTMLInputElement;
+
+    currBut!.addEventListener("click", function () {
+        if (currMenu!.classList.contains("hide")) {
+            currMenu!.classList.remove("hide");
+            currMenu.setAttribute("style","display: grid;")
+        } else {
+            currMenu!.classList.add("hide");
+            currMenu.setAttribute("style","display: none;")
+
+        }
+    });
+})();
