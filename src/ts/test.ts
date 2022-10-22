8286b15058061f9a4a44e8e7801ff17ba8031824
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-10-22 20:01:38 +0300

    function that return a html tag with currency change

--- src/ts/test.ts
+++ src/ts/test.ts
@@ -1,3 +1,17 @@
+const cards = document.querySelector(".cards");
 
-const a = document.querySelector<HTMLDivElement>(".p");
-a!.style.display = "none";
+function CurrencyCardDiff(diffAmount:number) {
+    // get arrow icon;
+    const arrow = diffAmount < 0
+        ? `<i class="fa fa-arrow-down" aria-hidden="true"></i>`
+        : `<i class="fa fa-arrow-up" aria-hidden="true"></i>`;
+
+    //generate class to display price increment / decrement;
+    let className = 'diff-amount';
+    diffAmount < 0 ? className += " red" : className += " green";
+
+    // return element to display on page;
+    return `<span class="${className}">
+        ${arrow} (${diffAmount})
+    </span>`
+}
