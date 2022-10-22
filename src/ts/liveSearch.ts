1fe97b5d094c187cee21990a768e0b47802ea10f
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-10-22 21:19:06 +0300

    live search script

---
+++ src/ts/liveSearch.ts
@@ -0,0 +1,17 @@
+const search = document.querySelector(".search") as HTMLInputElement;
+search!.addEventListener('input', () => {
+    let text: string = search!.value.trim().toLowerCase();
+    let cardArr = document.querySelectorAll(".card") ;
+    cardArr.forEach((elem=>{
+        if (text !== "") {
+                let cardTittle = elem.querySelector<HTMLInputElement>(".card__tittle");
+                if (cardTittle!.innerText.toLowerCase().search(text) === -1) {
+                    elem.classList.add("hide")
+                } else {
+                    elem.classList.remove("hide")
+                }
+        }else{
+            elem.classList.remove("hide");
+        }
+    }))
+});
