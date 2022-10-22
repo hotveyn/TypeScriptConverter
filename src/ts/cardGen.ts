de87ff5180ca0130c8a0b80c1ef5eab1457d0ec3
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-10-22 20:57:55 +0300

    fix bug in genCard
    not all textы in p.inc-form change theyself color

--- src/ts/cardGen.ts
+++ src/ts/cardGen.ts
@@ -1,4 +1,4 @@
-(()=>{
+(() => {
     const cards = document.querySelector(".cards");
 
     type oneOfValutes = {
@@ -14,47 +14,50 @@
         Valute: any[]
     }
 
-    function CurrencyCardDiff(diffAmount:number):string {
+    function CurrencyCardDiff(diffAmount: number, nominal: number, charCode: string): string {
         // get arrow icon;
-        const arrow = diffAmount < 0
+        const arrow: string = diffAmount < 0
             ? `<i class="fa fa-arrow-down" aria-hidden="true"></i>`
             : `<i class="fa fa-arrow-up" aria-hidden="true"></i>`;
 
         //generate class to display price increment / decrement;
-        let className = 'diff-amount';
+        let className: string = 'diff-amount';
         diffAmount < 0 ? className += " red" : className += " green";
 
         // return element to display on page;
-        return `<span class="${className}">
-        ${arrow} (${diffAmount})
-    </span>`
+        return `<p class="inc form ${className}" > 
+                ${nominal} ${charCode}
+                    <span >
+                        (${diffAmount}) ${arrow}
+                    </span>
+                    <div class="separate"></div>
+                </p>`
     }
 
-    function CurrencyCard(currency:oneOfValutes):string {
+    function CurrencyCard(currency: oneOfValutes): string {
         // get currency difference
-        const diff:string = (currency.Value - currency.Previous).toFixed(2);
-        return `<div">
+        const diff: string = (currency.Value - currency.Previous).toFixed(2);
+        return `
                 <div class="card">
                     <p class="card__tittle">${currency.Name}</p>
                     <div class="change-container" >
-                        <p class="inc form" > 1 ${currency.CharCode}
-                            ${CurrencyCardDiff(Number(diff))}
-                        <div class="separate"></div></p>
+                            ${CurrencyCardDiff(Number(diff), currency.Nominal, currency.CharCode)}
                         <p class="to">${currency.Value.toFixed(2)} RUB</p>
                     </div>
-                </div>
-            </div>`;
+                </div>`;
     }
-    function generateCards(cardsData:JSONWithValutes):void {
+
+    function generateCards(cardsData: JSONWithValutes): void {
         for (let key in cardsData.Valute) {
             cards!.innerHTML += CurrencyCard(cardsData.Valute[key]);
         }
     }
 
-    async function getCurrencies():Promise<void> {
+    async function getCurrencies(): Promise<void> {
         const res = await fetch("https://www.cbr-xml-daily.ru/daily_json.js");
         const json = await res.json();
         generateCards(json);
     }
+
     getCurrencies();
 })();
