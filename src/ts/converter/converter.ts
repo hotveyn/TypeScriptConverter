58f86450cefe2854eeb517083b74ff58e5c83ed3
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-10-24 23:00:49 +0300

    eto pizdec

--- src/ts/converter/converter.ts
+++ src/ts/converter/converter.ts
@@ -1,48 +1,41 @@
 (() => {
-    // type oneOfValutes = {
-    //     ID: string,
-    //     NumCode: string,
-    //     CharCode: string,
-    //     Nominal: number,
-    //     Name: string,
-    //     Value: number,
-    //     Previous: number
-    // }
-    // type JSONWithValutes = {
-    //     Valute: any[]
-    // }
 
-    let chosenValuteHave: string = "";
-    let chosenValuteWant: string = "";
+
+
+
 
     let haveValutesNode = document.querySelectorAll(".have__valute")!;
     let wantValutesNode = document.querySelectorAll(".want__valute")!;
 
-    function chooseValute(valutesNode: NodeListOf<Element>, chosenValute: string): void {
+    let chosenValuteHave: string = document.querySelector(".have__valutes .valute_chosen")!.textContent!.trim();
+    let chosenValuteWant: string = document.querySelector(".want__valutes .valute_chosen")!.textContent!.trim();
+
+    function chooseValute(valutesNode: NodeListOf<Element>, haveOrWant: string, valutesData:JSONWithValutes): void {
         // Make valute button green and chosen when user click on them
         valutesNode.forEach((valute) => {
             valute.addEventListener("click", () => {
-                chosenValute = valute.textContent!;
-
+                haveOrWant === "have" ?
+                    chosenValuteHave = valute.textContent!.trim() :
+                    chosenValuteWant = valute.textContent!.trim();
                 valutesNode.forEach((elem) => {
                     elem.classList.remove("valute_chosen");
                 })
+                valute.classList.add("valute_chosen");
 
-                valute.classList.toggle("valute_chosen");
-
-                console.log(chosenValute);
-            })
+                changeCurrency(valutesData);
+            });
         })
     }
 
-    function changeValutes(which:string): void {
+    function changeValutes(haveOrWant: string, valutesData:JSONWithValutes): void {
         // Valutes that was chosen in currency button will appear in have__valutes block
-        let currencyMenu = document.querySelector(`.${which}__currency-menu`)!;
-        let valutesNode = which === "have"? haveValutesNode: wantValutesNode;
+        let currencyMenu = document.querySelector(`.${haveOrWant}__currency-menu`)!;
+        let valutesNode = haveOrWant === "have" ? haveValutesNode : wantValutesNode;
         currencyMenu.childNodes.forEach((currP) => {
             currP.addEventListener("click", () => {
                 let ValuteArr: string[] = Array.from(valutesNode).map((a) => a.textContent!.trim());
-                if(!ValuteArr.includes(currP.textContent!.trim())){
+
+                if (!ValuteArr.includes(currP.textContent!.trim())) {
                     for (let i = ValuteArr.length - 1; i > 0; i--) {
                         ValuteArr[i] = ValuteArr[i - 1];
                     }
@@ -54,29 +47,75 @@
                         valutesNode[i].classList.remove("valute_chosen");
                     }
                     valutesNode[0].classList.add("valute_chosen")
-                }else{
+                    haveOrWant === "have" ?
+                        chosenValuteHave = valutesNode[0].textContent!.trim() :
+                        chosenValuteWant = valutesNode[0].textContent!.trim()
+
+                    changeCurrency(valutesData);
+                } else {
+                    // If currency is already on the valutes block just choose it
+                    valutesNode.forEach((elem) => {
+                        elem.classList.remove("valute_chosen")
+                    })
                     for (let i = 0; i < valutesNode.length; i++) {
-                        if(valutesNode[i].textContent!.trim() === currP.textContent!.trim()){
-                            valutesNode.forEach((elem)=>{
-                                elem.classList.remove("valute_chosen")
-                            })
+                        if (valutesNode[i].textContent!.trim() === currP.textContent!.trim()) {
                             valutesNode[i].classList.add("valute_chosen")
+                            haveOrWant === "have" ?
+                                chosenValuteHave = valutesNode[0].textContent!.trim() :
+                                chosenValuteWant = valutesNode[0].textContent!.trim()
+
                             break
                         }
                     }
+                    changeCurrency(valutesData);
                 }
             })
         })
     }
 
+    // type oneOfValutes = {
+    //     ID: string,
+    //     NumCode: string,
+    //     CharCode: string,
+    //     Nominal: number,
+    //     Name: string,
+    //     Value: number,
+    //     Previous: number
+    // }
+    type JSONWithValutes = {
+        Valute : any[]
+    }
+    function changeCurrency(valutesData:JSONWithValutes): void {
+        for (let key in valutesData.Valute){
+            if(valutesData.Valute[key].CharCode === chosenValuteHave ){
+                // let haveRate = document.querySelector(".have__rate")!;
+                // let haveCharCode = haveRate.querySelector(".have__char-code")
+                // let haveToNominal = haveRate.querySelector(".have__to-nominal")
+                // let haveToCharCode = haveRate.querySelector(".have__to-char-code")
+                // haveRate.innerHTML = `
+                //       <span class="have__nominal">1</span>
+                //       <span class="have__char-code">${valutesData.Valute[key].CharCode}</span> =
+                //       <span class="have__to-nominal">${(valutesData.Valute[key].Nominal / valutesData.Valute[key].Value).toFixed(2)}</span>
+                //       <span class="have__to-char-code">${valutesData.Valute[key].CharCode}</span>
+                //       `;
+            }if(valutesData.Valute[key].CharCode === chosenValuteWant ){
+            }
+        }
+    }
+
 
     async function getCurrencies(): Promise<void> {
-        //const res = await fetch("https://www.cbr-xml-daily.ru/daily_json.js");
-        //const json = await res.json();
-        await chooseValute(haveValutesNode, chosenValuteHave); // For Have block
-        await chooseValute(wantValutesNode, chosenValuteWant); // For Want block
-        await changeValutes("have");
-        await changeValutes("want");
+        const res = await fetch("https://www.cbr-xml-daily.ru/daily_json.js");
+        const json = await res.json();
+        changeCurrency(json);
+
+        chooseValute(haveValutesNode, "have",json); // For Have block
+        chooseValute(wantValutesNode, "want",json); // For Want block
+
+        changeValutes("have", json); // For Have block
+        changeValutes("want", json); // For Want block
+        // changeCurrencyRateWithClickOnValute("have", json);
+        // changeCurrencyRateWithClickOnValute("want", json);
     }
 
     getCurrencies();
