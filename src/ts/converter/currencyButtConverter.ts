2f5f8fbb7160ad4d9675ccb5d0d5a53273cc2bd5
kozakqwerty <61887221+hotveyn@users.noreply.github.com>
2022-10-24 14:14:10 +0300

    Add standart valute_chosen

--- src/ts/converter/currencyButtConverter.ts
+++ src/ts/converter/currencyButtConverter.ts
@@ -1,16 +1,16 @@
 (() => {
     const haveCurr = document.querySelector(".have__currency")!;
-    const haveCurrBut = haveCurr.querySelector(".currency-butt")! as HTMLInputElement;
-    const haveCurrMenu = haveCurr.querySelector(".currency-menu")! as HTMLInputElement;
+    const haveCurrBut = haveCurr.querySelector(".currency-butt")! as HTMLButtonElement;
+    const haveCurrMenu = haveCurr.querySelector(".currency-menu")! as HTMLButtonElement;
 
     const wantCurr = document.querySelector(".want__currency")!;
-    const wantCurrBut = wantCurr.querySelector(".currency__button")! as HTMLInputElement;
-    const wantCurrMenu = wantCurr.querySelector(".currency-menu")! as HTMLInputElement;
+    const wantCurrBut = wantCurr.querySelector(".currency__button")! as HTMLButtonElement;
+    const wantCurrMenu = wantCurr.querySelector(".currency-menu")! as HTMLButtonElement;
 
     ShowHide(haveCurrBut, haveCurrMenu);
     ShowHide(wantCurrBut, wantCurrMenu);
 
-    function ShowHide(but: HTMLInputElement, menu: HTMLInputElement) {
+    function ShowHide(but: HTMLButtonElement, menu: HTMLButtonElement) {
         but!.addEventListener("click", () => {
             if (menu!.classList.contains("hide")) {
                 menu!.classList.toggle("hide");
